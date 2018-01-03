f80901f24128457fcd6cf765e5216f79fd5a1308
nakodila <elenatalk@gmail.com>
2018-01-03 13:42:01 -0500

    added App and root components

---
+++ frontend/components/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+const App = () => (
+  <div>
+    <h1>Remember The Milk</h1>
+  </div>
+);
+
+export default App;
--- frontend/remember_milk.jsx
+++ frontend/remember_milk.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { login, logout, signup } from './util/session_api_util.js';
+import Root from './components/root';
 import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to Remember List</h1>, root);
-  window.login = login;
-  window.logout = logout;
-  window.signup = signup;
+  ReactDOM.render(<Root store={ store }/>, root);
   window.getState = store.getState;
   window.dispatch = store.dispatch;
 });
