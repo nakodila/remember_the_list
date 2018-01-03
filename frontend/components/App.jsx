4d8aee9c95e967cbc7155ca7733c1b03e880cdef
nakodila <elenatalk@gmail.com>
2018-01-03 17:36:33 -0500

    Added login form

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import HomepageContainer from './homepage/homepage_container';
+import SessionFormContainer from './session/session_form_container';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+
 
 const App = () => (
   <div>
     <header>
-      <h1>Remember The Milk</h1>
+      <h1>Remember The List</h1>
       <HomepageContainer />
     </header>
+    <Route path="/login" component={SessionFormContainer} />
+    <Route path="/signup" component={SessionFormContainer} />
   </div>
 );
 
--- frontend/remember_milk.jsx
+++ frontend/remember_milk.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
+import * as SessionActions from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
@@ -9,4 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<Root store={ store }/>, root);
   window.getState = store.getState;
   window.dispatch = store.dispatch;
+  window.login = SessionActions.login;
+  window.signup = SessionActions.signup;
+  window.logout = SessionActions.logout;
 });
