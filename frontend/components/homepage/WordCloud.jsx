b708117f3008b5fc8aecd43256bf12c6525458f3
nakodila <elenatalk@gmail.com>
2018-01-25 10:37:38 -0500

    started word clouds

---
+++ frontend/components/homepage/WordCloud.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const WordBubble = () => (
+  <div className="word-bubble">
+    <img src={window.staticImages.logoWhite} alt="logo" className="logo"/>
+    <p></p>
+  </div>
+);
+
+export default WordBubble;
--- frontend/components/homepage/homepage.jsx
+++ frontend/components/homepage/homepage.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import ListIndexContainer from '../list/list_index_container';
 import ListShowContainer from '../list/list_show_container';
-
 import TaskIndexContainer from '../task/task_index_container';
-import TaskShowContainer from '../task/task_show_container'
+import TaskShowContainer from '../task/task_show_container';
+import WordBubble from './bob_words';
 
 const sessionLinks = () => (
     <main className="homepage-main">
@@ -23,6 +23,8 @@ const sessionLinks = () => (
         <Link to="/signup" className="red-signup-btn">Sign Up Free</Link>
       </div>
       <div className="signup-art">
+        <WordBubble words="Write the list in the morning" />
+        <WordBubble words="Be the best you can be at 5pm" />
         <img src="https://i.imgur.com/DBe6uid.png" alt="Dont forget to call Bob"/>
       </div>
       <footer className="footer"></footer>
